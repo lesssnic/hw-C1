f2d605c786361f32519bd2d664b36b3d500b22ef
lesssnic <antoxxxa.ua@gmail.com>
2021-06-05 13:18:31 +0300

    add-table

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 
 
-let monthCredit = 60;
+function creditCalc() {
 
-let sumCredit = 59999999900;
+let monthCredit = +duration.value;
 
-let persentCreditRate = 25;
+let sumCredit = +creditSum.value;
+
+let persentCreditRate = +percent.value;
 
 let K = ((1 + (persentCreditRate/1200))**monthCredit); //Введено для упрощения читаемости формулы рассчета monthPayment
 
@@ -12,30 +14,24 @@ let monthPayment = Math.round((sumCredit * ((persentCreditRate/1200) * K / (K - 
 
 let totalOverpay = 0;
 
-console.log('Сумма кредита:  ' + sumCredit + '  Ежемесячный платеж:  ' + monthPayment);
+let arrToHtml = [];
+
+//console.log('Сумма кредита:  ' + sumCredit + '  Ежемесячный платеж:  ' + monthPayment);
 
     for(i = 1; i <= monthCredit; i++){
 
+        let monthPercentPayment = Math.round((sumCredit * (persentCreditRate/1200)*100))/100;
+        let monthBodyPayment = Math.round((monthPayment - monthPercentPayment)*100)/100;
+        totalOverpay += monthPercentPayment;
+        
         if(i < monthCredit){
         
-        let monthPercentPayment = Math.round((sumCredit * (persentCreditRate/1200)*100))/100;
-
-        totalOverpay += monthPercentPayment;
-
-        let monthBodyPayment = Math.round((monthPayment - monthPercentPayment)*100)/100;
-
         sumCredit = Math.round((sumCredit - monthBodyPayment)*100)/100;
 
-        console.log(i + '  Погашение тела кредита:  ' + monthBodyPayment + '  Погашение процентов по кредиту:  ' + monthPercentPayment + '  В сумме: ' + monthPayment);
-        console.log('Тело кредита к началу следующего месяца:  ' + sumCredit);
+        arrToHtml.push(`<tr><th>${i}</th> <th>${monthBodyPayment}</th> <th>${monthPercentPayment}</th> <th>${monthPayment}</th> <th>${sumCredit}</th></tr>`);
+        //console.log('Тело кредита к началу следующего месяца:  ' + sumCredit);
         
         }else{
-
-        let monthPercentPayment = Math.round((sumCredit * (persentCreditRate/1200)*100))/100;
-
-        totalOverpay += monthPercentPayment;
-
-        let monthBodyPayment = Math.round((monthPayment - monthPercentPayment)*100)/100;
 
         sumCredit = Math.round((sumCredit - monthBodyPayment)*100)/100;
 
@@ -44,12 +40,16 @@ console.log('Сумма кредита:  ' + sumCredit + '  Ежемесячны
         let lastMonthPayment = Math.round((monthBodyPayment + monthPercentPayment)*100)/100;
 
         sumCredit = 0;
-
-        console.log(i + '  Погашение тела кредита:  ' + monthBodyPayment + '  Погашение процентов по кредиту:  ' + monthPercentPayment + '  В сумме:  ' + lastMonthPayment);
-        console.log('Тело кредита к началу следующего месяца:  ' + sumCredit);
+        
+        arrToHtml.push(`<tr><th>${i}</th> <th>${monthBodyPayment}</th> <th>${monthPercentPayment}</th> <th>${lastMonthPayment}</th> <th>${sumCredit}</th></tr>`);
+        //arrToHtml.push(i + '  Погашение тела кредита:  ' + monthBodyPayment + '  Погашение процентов по кредиту:  ' + monthPercentPayment + '  В сумме:  ' + lastMonthPayment);
+        //console.log('Тело кредита к началу следующего месяца:  ' + sumCredit);
 
         }
 
     }
 
-console.log('Переплата по кредиту составит: ' + Math.round(totalOverpay*100)/100 + '  грн.');
+    overpay.value = totalOverpay.toFixed(2) + '   ГРН.'
+    schedulePayments.innerHTML = arrToHtml.join('');
+    //document.write('Переплата по кредиту составит: ' + Math.round(totalOverpay*100)/100 + '  грн.');
+}
